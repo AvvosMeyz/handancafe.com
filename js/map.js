aa7580a690656f5c1e81f311869aef693fad5bb6
Avvos <hmztkn@gmail.com>
2017-11-03 04:09:47 +0300

    Added sass files

--- js/map.js
+++ js/map.js
@@ -5,14 +5,14 @@ AOS.init({
 
 var map;
 function initMap() {
-    var dukkan = {lat: 38.392973, lng: 27.038170};
+    var my_loc = {lat: 38.392973, lng: 27.038170};
     map = new google.maps.Map(document.getElementById('googleMap'), {
-    center: dukkan,
+    center: my_loc,
     zoom: 15
     
 });
 var marker = new google.maps.Marker({
-      position: dukkan,
+      position: my_loc,
       map: map
     });
 }
